bc0d30d9bcff62493f42d2700b3b18b7d7e04f48
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-04-24 22:35:22 +0300

    add functinality method inside password vault

--- lock/vault.py
+++ lock/vault.py
@@ -9,3 +9,35 @@ class Password_vault:
         self.site = site
         self.regesterdBy = regesterdBy
         self.site_password = site_password
+
+
+
+    #create credentials for account
+    def save_credentials(self):
+        """
+         this function creates new accounts in password vault
+         """
+        Password_vault.accounts_password.append(self)
+    
+    def delete_credentials(self):
+        """
+          this function to delets users account details from the password vault
+         """
+        
+        Password_vault.accounts_password.remove(self)  
+    
+    
+    
+    @classmethod 
+    
+    def display_credentials(cls):
+        
+        
+        """"
+        this method show s all theaccounts and password inside password vaul
+        
+        """
+        
+        return cls.accounts_password
+        
+          
