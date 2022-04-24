2eaa0f447189c2c2182a48a5089eea9306d148c3
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-04-24 20:42:28 +0300

    add docstring to explain my code

--- user.py
+++ user.py
@@ -1,4 +1,12 @@
 class User:
+    ''''
+    add a class variable that hold user details
+    '''
+    user_detail = []
+
+    ''''
+    add an instance to the class using the constructor
+    '''
     def __init__(self,username, password):
         self.username =username
         self.password =password
