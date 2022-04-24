234a2249aacc312e5b77ac6bef19b55149551cb7
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-04-25 00:36:21 +0300

    fix a bug

--- lock/cred_test.py
+++ lock/cred_test.py
@@ -7,7 +7,7 @@ class TestCredentials(unittest.TestCase):
     A test class that defines test cases for in password vault class
 
     """ 
-    def setUp(self):
+    def setCreds(self):
         """
         Method creates an object from password vault and
         runs before the  other test case
@@ -18,6 +18,15 @@ class TestCredentials(unittest.TestCase):
 
 
 
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.site,'twitter')
+        self.assertEqual(self.new_credential.regesterdBy,'jules')
+        self.assertEqual(self.new_credential.site_password,'1234')
+
+    
 
 
 
@@ -30,9 +39,6 @@ class TestCredentials(unittest.TestCase):
 
 
 
+if __name__ == '__main__':
+    unittest.main()   
 
-
-
-
-if __name__ == '__main___':
-       unittest.main()
