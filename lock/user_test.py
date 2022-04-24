1d954dd9724006e0cfb182effc46560dbed7416d
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-04-25 00:13:23 +0300

    check if the object property are well intitialized

--- lock/user_test.py
+++ lock/user_test.py
@@ -3,3 +3,23 @@ import unittest
 from user import User  # Importing the User and Credentials class
 
 
+
+class TestUser(unittest.TestCase):
+    
+  
+    
+    def setUp(self):
+        
+        '''
+        this method runs before other test case method
+        this method creates an object from the class user
+        '''
+        self.new_user = User("jules","1234") 
+
+    def test_properties(self):
+        '''
+        this method checks if the objects property are
+        correctly initialized
+        '''
+        self.assertEqual(self.new_user.username,'jules')
+        self.assertEqual(self.new_user.password,'123')
