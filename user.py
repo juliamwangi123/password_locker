6f424b274b31aeb87b2ba2f1412d3843cf8d58f6
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-04-24 20:45:40 +0300

    ft:add method that adds new user

--- user.py
+++ user.py
@@ -4,10 +4,17 @@ class User:
     '''
     user_detail = []
 
-    ''''
-    add an instance to the class using the constructor
-    '''
+    
     def __init__(self,username, password):
+        ''''
+        add an instance properties  using the constructor
+        '''
         self.username =username
         self.password =password
-        
+    
+    def save_user(self):
+        """
+        add  new user to the list using append method
+        """
+        User.user_detail.append(self)
+    
