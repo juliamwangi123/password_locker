744aec71cf50b267c25ea9d7dec7680c8485a5cf
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-04-25 18:38:29 +0300

    fr:proptuser to create or autogenerate password

--- lock/account.py
+++ lock/account.py
@@ -47,92 +47,25 @@ def generate_Password():
   
 
 def show():
-    
     while True:
-        print("To Register click 'REG' to login 'LOG' ")
+        print("Hello Welcome to your password vault select 'REG to Create New Account  'LI' to login into your accoutn")
+        code=input("Enter code:..")
+        if code == "REG":
+            print("Create acount")
+        username = input("Your Name: ")
+        print("for automated passcode press 'auto' for custom password press 'cust' ")
+        selection= input("Enter password selection:....")
 
-        code = input().upper()
-        print()
-        print()
-        
-        
-        if code == 'REG':
-            
-            print("Enter Username:")
-            username = input()
-            
-            print("Create your Password")
-            password= input()
-            
-            print("Confirm your Password")
-            confirm_password = input()
-            
-            if confirm_password != password:
-                print("Your password did not match.Try Again")
-                password = input()
-                print("Confirm Your password")
-                confirm_password = input()
-                
+        while True:
+            if selection == 'cust':
+                    password = input("Enter Password")
+                    break
+            elif selection == 'auto':
+                    password = generate_Password()
+                    break
             else:
-                print("Succesfully Registerd")
-                print("LOGIN")
-                print("Enter your Username")
-                name = input()
-                print("Enter Password")
-                user_pass= input()
-                
-                
-            while name != username or user_pass != password:
-                print("Your Username or Password is invalid")
-                print("Re-enter your Username")
-                name = input()
-                print("Enter your Password")
-                user_pass = input()
-                
-            else:
-                print(" you've logged in succesfully")
-                
-        elif code == 'L':
-            print("Welcome, Login to your Account")
-            print("Enter your Username")
-            saved_username = input()
-            print("Enter Password")
-            saved_password = input()
-            
-            if saved_username != "jules" or saved_password != "test":
-                 print("Invalid username or password. Username is 'jules' and Password is 'test'")
-                 print("Enter your Username")
-                 saved_username= input()
-                 print("Enter Password")
-                 saved_password = input()
-                 
-            else:
-                print("Logged in Succesfully in your Password ")
-
-                print(">>>>>>>>>>>>")
-                print("To register new acc press 'add', to display saved item 'display'  and 'delete' to delete")
-                pass_code = input("pick from the selection")
-                if pass_code == "add":
-                    print("Create New site")
-                    print("Website")
-                    site = input().lower()
-                    print("Your site username")
-                    site_userName = input()
-                    print("type C for creating your password or G for us to generate")
-                    pass_gen=input()
-                    if pass_gen == "C":
-                        create_password=input("input password:")
-                    else:
-                        g_pass=generate_Password()
-
-        
-        elif code == 'EXIT':
-            break
-        
-        else:
-            
-            print("We did not recgnize the code picked")  
-            
+                print("Invalid password please try again")
+     
             
 if __name__ == '__main__':
 
